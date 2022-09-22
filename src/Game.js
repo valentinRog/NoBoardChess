52cdcbfd28117d42a0e00be2b24077d79a8c3877
valentinRog <valentin.rogister@gmail.com>
2022-09-22 16:25:31 +0200

    getPuzzle in class

--- src/Game.js
+++ src/Game.js
@@ -7,6 +7,19 @@ class Game {
     this.lives = 3;
     this.fails = 0;
   }
+
+  async getPuzzle() {
+    const res = await fetch(
+      `https://valenbel123.pythonanywhere.com/level/${this.level}`
+    );
+    const puzzle = await res.json();
+    if (res.ok) {
+      console.log(puzzle);
+      return puzzle;
+    } else {
+      throw new Error(puzzle);
+    }
+  }
 }
 
 export default Game;
--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -3,28 +3,14 @@
   import Infos from "./Infos.svelte";
   import Guessing from "./Guessing.svelte";
 
-  let level = 1;
-
-  const get_puzzle = async () => {
-    const res = await fetch(
-      `https://valenbel123.pythonanywhere.com/level/${level}`
-    );
-    const puzzle = await res.json();
-    if (res.ok) {
-      console.log(puzzle);
-      return puzzle;
-    } else {
-      throw new Error(puzzle);
-    }
-  };
-  let promise = get_puzzle();
-
   import Game from "../Game";
   let game = new Game();
 
+  let promise;
+  let level;
   $: if (level != game.level) {
     level = game.level;
-    promise = get_puzzle();
+    promise = game.getPuzzle();
   }
 </script>
 
